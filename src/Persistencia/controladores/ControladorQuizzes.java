0bf327564146af59311b3e46551a21a0c4644481
mgrabysz <margrabysz@gmail.com>
2022-03-18 17:13:34 +0100

    Empezar a realizar creacion del quiz aleatorio

--- src/Persistencia/controladores/ControladorQuizzes.java
+++ src/Persistencia/controladores/ControladorQuizzes.java
@@ -4,11 +4,14 @@
  */
 package Persistencia.controladores;
 
+import LogicaNegocio.modelo.Pregunta;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import Persistencia.conexion.Conexion;
 import LogicaNegocio.modelo.Quiz;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class ControladorQuizzes {
     MongoDatabase db;
@@ -20,5 +23,22 @@ public class ControladorQuizzes {
         this.quizzes = db.getCollection("Quizzes");
     }
     
+    public void barajarListaPreguntas(ArrayList<Pregunta> lista) {
+        // shuffle ArrayList
+	Collections.shuffle(lista);
+    }
+    
+    public void crearQuizAleatorio(int num, String nombre, ArrayList<Pregunta> lista) {
+        Collections.shuffle(lista);
+        ArrayList<Pregunta> nuevaLista = new ArrayList<Pregunta>();
+        
+        for(int i = 0; i< num; i++)
+            nuevaLista.add(lista.get(i));
+        
+        // TODO:
+        // crear quiz con preguntas de nuevaLista
+        // subir a base de dato
+    }
+     
     
 }
